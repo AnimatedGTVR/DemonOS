5c9d7b669abcbae97de120b6aaf17822c97d0628
AnimatedGTVR <jaxsonjerryb@gmail.com>
2026-07-30 07:29:10 -0400

    I really dont know at this  point
    1: XLIB full port
    
    2: remake of desktop and remove PekWM and EDDE

--- sidelined/Desktop/pekwm/test/data/cfg_parser_command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Simple script outputting configuration parsed in test_CfgParser.hh
-#
-
-cat <<EOF
-key = "value"
-EOF
--- sidelined/Desktop/pekwm/test/system/pekwm_sys.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DISPLAY=`grep 'global DISPLAY=' test.pluxinc| sed 's/.*=//' | sed 's/]//'`
-PEKWM_CONFIG_FILE=$PWD/config/pekwm.config.sys
-export DISPLAY PEKWM_CONFIG_FILE
-$VALGRIND ../../build/src/sys/pekwm_sys --log-level trace
